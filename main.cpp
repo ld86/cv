e6cfb39f2c9e9600d65f6efcb5da56c544ba470d
Bogdan Melnik <ld@hackerdom.ru>
2015-01-08 14:12:07 +0500

    Canny edges

--- main.cpp
+++ main.cpp
@@ -9,9 +9,22 @@ int main(int argc, char** argv) {
     cv::VideoCapture cap(from);
     cv::namedWindow(from,1);
     while (cap.isOpened()) {
-        cv::Mat frame;
-        cap >> frame;
-        cv::imshow(from, frame);
+        cv::Mat origin, gray;
+        cap >> origin;
+        cv::cvtColor(origin, gray, CV_BGR2GRAY );
+
+        // cv::Mat smooth(frame);
+        // cv::GaussianBlur(frame, smooth, cv::Size(3, 3), 0, 0);
+        cv::Mat edges;
+        cv::blur(gray, edges, cv::Size(3,3));
+
+        cv::Canny(edges, edges, 100, 300, 3);
+
+        cv::Mat result;
+        result = cv::Scalar::all(0);
+        origin.copyTo(result, edges);
+
+        cv::imshow(from, result);
         if(cv::waitKey(30) >= 0)
             break;
     }
